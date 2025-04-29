2b9a36c45c5d2519e21ddf21b8eb0a6d768f27be
jupas <juapasfer2@alu.edu.gva.es>
2025-04-29 19:39:37 +0200

    v1.0.3

--- sistema_monitorizacion/test/widget_test.dart
+++ sistema_monitorizacion/test/widget_test.dart
@@ -5,6 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+// ignore: unused_import
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:provider/provider.dart';
